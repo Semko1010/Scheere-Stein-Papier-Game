d8b44ccd0fe2088b49d1b10c8c1532959de5996b
Semko1010 <semir01020@gmail.com>
2021-12-03 12:21:14 +0100

    v1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,50 @@
-let imgArr=[
+let imgArr = [
     "assets/img/rock.png",
     "assets/img/paper.png",
     "assets/img/scissors.png"
 ];
-
 let compImg = document.querySelector("#resultsUserImg img");
-let resultsUserImgPoints= document.querySelector("#resultsUserImgPoints");
+let resultsUserImgPoints = document.querySelector("#resultsUserImgPoints");
+let resultsUser = document.querySelector("#resultsUser");
+let resultsComp = document.querySelector("#resultsComp");
 
-function enter(a){
+let resultUser = 0;
+let resultComp = 0;
+
+function enter(a) {
     let rnd = Math.floor(Math.random() * imgArr.length);
-    compImg.setAttribute("src",imgArr[rnd]);
-    if(a ==rnd) {
-        resultsUserImgPoints.innerHTML="Again";
-    }if(a==0 && rnd==1){
-        resultsUserImgPoints.innerHTML="You lose";
-    }if(a==0 && rnd==2){
-        resultsUserImgPoints.innerHTML="You win";
-    }if(a==1 && rnd==0){
-        resultsUserImgPoints.innerHTML="You win";
-    }if(a==1 && rnd==2){
-        resultsUserImgPoints.innerHTML="You lose";
-    }if(a==2 && rnd==0){
-        resultsUserImgPoints.innerHTML="You lose";
-    }if(a==2 && rnd==1){
-        resultsUserImgPoints.innerHTML="You win";
+    compImg.setAttribute("src", imgArr[rnd]);
+    if (a == rnd) {
+        resultsUserImgPoints.innerHTML = "Again";
+    }
+    if (a == 0 && rnd == 1) {
+        resultsUserImgPoints.innerHTML = "You lose";
+        resultComp += 1;
+        resultsComp.innerHTML = resultComp
+    }
+    if (a == 0 && rnd == 2) {
+        resultsUserImgPoints.innerHTML = "You win";
+        resultUser += 1;
+        resultsUser.innerHTML = resultUser
+    }
+    if (a == 1 && rnd == 0) {
+        resultsUserImgPoints.innerHTML = "You win";
+        resultUser += 1;
+        resultsUser.innerHTML = resultUser
+    }
+    if (a == 1 && rnd == 2) {
+        resultsUserImgPoints.innerHTML = "You lose";
+        resultComp += 1;
+        resultsComp.innerHTML = resultComp
+    }
+    if (a == 2 && rnd == 0) {
+        resultsUserImgPoints.innerHTML = "You lose";
+        resultComp += 1;
+        resultsComp.innerHTML = resultComp
+    }
+    if (a == 2 && rnd == 1) {
+        resultsUserImgPoints.innerHTML = "You win";
+        resultUser += 1;
+        resultsUser.innerHTML = resultUser
     }
 }
