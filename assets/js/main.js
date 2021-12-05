a1f5dbbb6c14e434a36f89f4611e7ad796bdf5a8
Semko1010 <semir01020@gmail.com>
2021-12-05 20:07:35 +0100

    v1.8

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,20 @@ let compImg = document.querySelector("#resultsUserImg img");
 let resultsUserImgPoints = document.querySelector("#resultsUserImgPoints");
 let resultsUser = document.querySelector("#resultsUser");
 let resultsComp = document.querySelector("#resultsComp");
+let rounds = document.querySelector("#rounds");
+let buttons = document.querySelector("#buttons");
+let buttons1 = document.querySelector("#buttons1");
 
 let resultUser = 0;
 let resultComp = 0;
-
+let trys=0;
 function enter(a) {
     let rnd = Math.floor(Math.random() * imgArr.length);
+    trys+=1;
     compImg.setAttribute("src", imgArr[rnd]);
     if (a == rnd) {
-        resultsUserImgPoints.innerHTML = "Again";
+        resultsUserImgPoints.innerHTML = "Draw";
+        trys-=1;
     }
     if (a == 0 && rnd == 1) {
         resultsUserImgPoints.innerHTML = "You lose";
@@ -47,4 +52,20 @@ function enter(a) {
         resultUser += 1;
         resultsUser.innerHTML = resultUser
     }
+    if(trys==rounds.value){
+        buttons.style.display = "none"
+        buttons1.style.display = "block"
+        if(resultUser<resultComp){
+            resultsUserImgPoints.innerHTML="Computer Wins";
+            resultsUserImgPoints.style.color="red";
+        }if(resultUser>resultComp){
+            resultsUserImgPoints.innerHTML="User Wins";
+            resultsUserImgPoints.style.color="green";
+        }else{
+            resultsUserImgPoints.innerHTML="Draw";
+        }
+    }
 }
+function reload(){
+    location.reload();
+};
